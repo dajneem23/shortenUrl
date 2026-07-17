893b9a7312ab94397c2990d169c6d3d9e1248037
dajneem23 <dajneem23@gmail.com>
2026-07-17 16:14:32 +0700

    fix

--- shorten-api/src/url/dto/create-url.dto.ts
+++ shorten-api/src/url/dto/create-url.dto.ts
@@ -1,7 +1,7 @@
-import { IsUrl, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength, MinLength, Matches } from 'class-validator';
 
 export class CreateUrlDto {
-  @IsUrl({ protocols: ['http', 'https'] }, { message: 'original_url must be a valid HTTP or HTTPS URL' })
+  @Matches(/^https?:\/\/\S+$/i, { message: 'original_url must be a valid HTTP or HTTPS URL' })
   originalUrl: string;
 
   @IsOptional()
